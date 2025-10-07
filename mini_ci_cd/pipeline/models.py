c52478ce8eeafbe1d4248c28d6bf3cdab74a5684
RinchhenThing <lamakiso588@gmail.com>
2025-10-07 11:38:06 +0545

    Added the models and views for the ci cd pipeline

--- mini_ci_cd/pipeline/models.py
+++ mini_ci_cd/pipeline/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Repository(models.Model):
+    name = models.CharField(max_length=100)
+    repo_url = models.URLField()
+    branch = models.CharField(max_length=100, default="main")
+
+    def __str__(self):
+        return self.name
+
+class Build(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('running', 'Running'),
+        ('success', 'Success'),
+        ('failed', 'Failed'),
+    ]
+
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    commit_id = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    log = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.repository.name} - {self.commit_id[:7]} - {self.status}"
--- mini_ci_cd/pipeline/views.py
+++ mini_ci_cd/pipeline/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import json 
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt 
+from .models import Repository, Build
 
-# Create your views here.
+@csrf_exempt 
+def github_webhook(request):
+    if request.method == 'POST':
+        try:
+            payload = json.loads(request.body)
+            repo_name = payload['repository']['name']
+            repo_url = payload['repository']['html_url']
+            commit_id = payload['after']
+
+            repo, _ = Repository.objects.get_or_create(name=repo_name, repo_url=repo_url)
+            build = Build.objects.create(repository=repo, commit_id=commit_id, status='pending')
+
+            #For now, we'll simulate a simple build
+            build.status = 'running'
+            build.log = "Build started.. \n"
+            build.save()
+
+            #later we'll replace this with actual commands
+            build.log += 'Simulating build.. \nBuild successful'
+            build.status = 'success'
+            build.save()
+
+            return JsonResponse({'message': 'Build triggered successfully'}, status=200)
+
+        except Exception as e:
+            print(e)
+            return JsonResponse({'error':str(e)}, status=400)
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
